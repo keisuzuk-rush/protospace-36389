1905155acf81373de0bdeb7ce7a1f60994441e1d
keisuzuk-rush <84221784+keisuzuk-rush@users.noreply.github.com>
2021-08-10 15:33:04 +0900

    Issue修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,9 +30,9 @@ class PrototypesController < ApplicationController
 	end
 
 	def update
-		prototype = Prototype.find(params[:id])
-		if prototype.update(prototype_params)
-			redirect_to root_path
+		@prototype = Prototype.find(params[:id])
+		if @prototype.update(prototype_params)
+			redirect_to prototype_path
 		else
 			render :edit
 		end
@@ -52,7 +52,8 @@ class PrototypesController < ApplicationController
 	end
 
 	def move_to_index
-    unless user_signed_in?
+		@prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
